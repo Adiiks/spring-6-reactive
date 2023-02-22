ae864c63bcf697679d3a7b173f54da6b26a71b49
Adiks <apacholak97@interia.pl>
2023-02-22 09:52:19 +0100

    add create and update date with auditing

--- src/test/java/pl/adrian/spring6reactive/repositories/BeerRepositoryTest.java
+++ src/test/java/pl/adrian/spring6reactive/repositories/BeerRepositoryTest.java
@@ -3,11 +3,14 @@ package pl.adrian.spring6reactive.repositories;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
+import pl.adrian.spring6reactive.config.DatabaseConfig;
 import pl.adrian.spring6reactive.domain.Beer;
 
 import java.math.BigDecimal;
 
 @DataR2dbcTest
+@Import(DatabaseConfig.class)
 class BeerRepositoryTest {
 
     @Autowired
