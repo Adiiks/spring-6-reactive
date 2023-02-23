fbb3ccd742d9712a319c4da937f44e2cd6d47494
Adiks <apacholak97@interia.pl>
2023-02-23 14:10:00 +0100

    test list beers

---
+++ src/test/java/pl/adrian/spring6reactive/controllers/BeerControllerTest.java
@@ -0,0 +1,26 @@
+package pl.adrian.spring6reactive.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.hamcrest.Matchers.hasSize;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class BeerControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    void testListBeers() {
+        webTestClient.get().uri(BeerController.BEER_PATH)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectBody().jsonPath("$", hasSize(3));
+    }
+}
